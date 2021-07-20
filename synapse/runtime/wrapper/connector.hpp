e339ce9266e2d881e6262c978988a1b7b509aa93
João Maria Tiago <joao.marques.tiago@tecnico.ulisboa.pt>
2021-07-20 11:58:51 +0100

    add missing extern to connector wrapper

--- synapse/runtime/wrapper/connector.hpp
+++ synapse/runtime/wrapper/connector.hpp
@@ -11,6 +11,10 @@
 namespace synapse::runtime {
 #endif // __cplusplus
 
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
 conn_t *synapse_runtime_connector_new(const char *grpc_addr,
                                       const char *p4_info_filepath,
                                       logging_level_t logging_level);
@@ -22,6 +26,10 @@ void synapse_runtime_connector_destroy(conn_t *connector);
 
 #ifdef __cplusplus
 } // namespace synapse::runtime
+#endif // __cplusplus
+
+#ifdef __cplusplus
+}
 #endif // __cplusplus
 
 #endif // SYNAPSE_RUNTIME_WRAPPER_CONNECTOR_HPP_
